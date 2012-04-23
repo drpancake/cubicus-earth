da90b0fb5a3c96448cab83fa1115119467129b5c
James Potter <james.potter@gmail.com>
2012-04-23 13:02:18 +0100

    Added app-wide constants

---
+++ CubicusEarth/CubicusEarth/CubicusEarth.h
@@ -0,0 +1,13 @@
+//
+//  CubicusEarth.h
+//  CubicusEarth
+//
+//  Created by James Potter on 23/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+extern NSString *const CE_DAEMON_HOST;
+extern NSString *const CE_APP_NAME;
+extern int const CE_DAEMON_PORT;
+
+extern int const CE_EARTH_CONTEXT;
---
+++ CubicusEarth/CubicusEarth/CubicusEarth.m
@@ -0,0 +1,15 @@
+//
+//  CubicusEarth.m
+//  CubicusEarth
+//
+//  Created by James Potter on 23/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CubicusEarth.h"
+
+NSString *const CE_DAEMON_HOST = @"localhost";
+NSString *const CE_APP_NAME = @"Cubicus Earth";
+int const CE_DAEMON_PORT = 28739;
+
+int const CE_EARTH_CONTEXT = 1;
